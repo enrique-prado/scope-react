0e0e90f491b8435b79fac5c1c5780cdc0fb4659d
Enrique Prado <enrique.prado7@gmail.com>
2016-04-12 12:18:59 -0700

    Replacing react-data-grid with material-ui table component
    Part 1

--- app/components/HoursTable.js
+++ app/components/HoursTable.js
@@ -1,5 +1,23 @@
 var React = require('react');
-var ReactDataGrid = require('react-data-grid/addons');
+import Table from 'material-ui/lib/table/table';
+import TableHeaderColumn from 'material-ui/lib/table/table-header-column';
+import TableRow from 'material-ui/lib/table/table-row';
+import TableHeader from 'material-ui/lib/table/table-header';
+import TableRowColumn from 'material-ui/lib/table/table-row-column';
+import TableBody from 'material-ui/lib/table/table-body';
+import TableFooter from 'material-ui/lib/table/table-footer';
+import TextField from 'material-ui/lib/text-field';
+
+const styles = {
+  propContainerStyle: {
+    width: 200,
+    overflow: 'hidden',
+    margin: '20px auto 0',
+  },
+  propToggleHeader: {
+    margin: '20px auto 10px',
+  },
+};
 
 //Columns definition
 var columns = [
@@ -35,7 +53,7 @@ var HoursTable = React.createClass({
     onHrsUpdate: React.PropTypes.func,
   },
 
-  rowGetter : function(rowIdx){
+  handleRowSelected : function(rowIdx){
     return this.props.rows[rowIdx]
   },
 
@@ -48,16 +66,38 @@ var HoursTable = React.createClass({
 
   render:function(){
     var rowsCount = this.props.rows ? this.props.rows.length : 0;
+    var hrs_list = this.props.rows.map(function(hour, index) {
+        return (
+            <TableRow key={index}>
+                <TableRowColumn>{hour.off}</TableRowColumn>
+                <TableRowColumn>{hour.day}</TableRowColumn>
+                <TableRowColumn>{hour.open}</TableRowColumn>
+                <TableRowColumn>{hour.close}</TableRowColumn>
+            </TableRow>
+        )
+    });
     
     return(
-      <ReactDataGrid
-      enableCellSelect={true}
-      columns={columns}
-      rowGetter={this.rowGetter}
+      <Table
+      height="300px"
+      selectable={true}
+      onRowSelection={this._onRowSelection}
       rowsCount={rowsCount}
       minHeight={500}
-     />
-    )
+      >
+        <TableHeader>
+            <TableRow>
+                <TableHeaderColumn tooltip="Day Off when checked">Day off</TableHeaderColumn>
+                <TableHeaderColumn tooltip="Day of the week">Day</TableHeaderColumn>
+                <TableHeaderColumn tooltip="Start of work hours">Open</TableHeaderColumn>
+                <TableHeaderColumn tooltip="End of work hours">Close</TableHeaderColumn>
+            </TableRow>
+        </TableHeader>
+        <TableBody deselectOnClickaway={true} showRowHover={true} >
+            {hrs_list}
+        </TableBody>
+      </Table>        
+    );
   }
 
 });
