75e850df0a6c24dc51b390fa265249b13c01a53f
Enrique Prado <enrique.prado7@gmail.com>
2016-04-29 17:06:16 -0700

    Commenting out dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: [
+        //'webpack-dev-server/client?http://localhost:8080',        
         'webpack/hot/only-dev-server',
         './app/index.js'
     ],
